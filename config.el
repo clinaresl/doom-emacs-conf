7ba321eed50a3e7493bd41aecfb7b46c42608c8c
Carlos Linares López <clinaresl@eservicios.indra.es>
2019-12-19 23:18:41 +0100

    I added a file for introducing key-bindings and other minor changes

--- config.el
+++ config.el
@@ -2,28 +2,10 @@
 
 ;; Place your private configuration here
 (setq
- projectile-project-search-path '("~/lab/" "~/uc3m/"))
+ projectile-project-search-path '("~/lab/" "~/uc3m/" "~/org/"))
 
-;; ;; global configuration parameters
-;; (global-set-key "\C-z" 'scroll-one-line-up)
-;; (global-set-key "\M-z" 'scroll-one-line-down)
+;; Load personal key bindings
+(load! "bindings")
 
-;; ;; Show the function name in those modes supported
-;; (which-function-mode)
-
-;; ;; Always end a file with a newline
-;; (setq require-final-newline t)
-
-;; ;; Stop at the end of the file, not just add lines
-;; (setq next-line-add-newlines nil)
-
-;; ;; functions used to scroll line by line
-;; (defun scroll-one-line-up (&optional arg)
-;;   "Scroll the selected window up (forward in the text) one line (or N lines)."
-;;   (interactive "p")
-;;   (scroll-up (or arg 1)))
-
-;; (defun scroll-one-line-down (&optional arg)
-;;   "Scroll the selected window down (backward in the text) one line (or N)."
-;;   (interactive "p")
-;;   (scroll-down (or arg 1)))
+;; manual selection of themes
+(load-theme 'doom-spacegrey t)
--- init.el
+++ init.el
@@ -66,7 +66,7 @@
        :term
        ;;eshell            ; a consistent, cross-platform shell (WIP)
        ;;shell             ; a terminal REPL for Emacs
-       ;;term              ; terminals in Emacs
+       term              ; terminals in Emacs
        ;;vterm             ; another terminals in Emacs
 
        :tools
@@ -97,7 +97,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;assembly          ; assembly for fun or debugging
-       ;;cc                ; C/C++/Obj-C madness
+       cc                ; C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -111,7 +111,7 @@
        ;;ess               ; emacs speaks statistics
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp           ; ML stands for Microsoft's Language
-       ;;go                ; the hipster dialect
+       go                ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
@@ -119,7 +119,7 @@
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       ;;latex             ; writing papers in Emacs has never been so fun
+       latex             ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
@@ -139,7 +139,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       ;;python            ; beautiful is better than ugly
+       python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
@@ -169,3 +169,17 @@
        :config
        ;;literate
        (default +bindings +smartparens))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4a9f595fbffd36fe51d5dd3475860ae8c17447272cf35eb31a00f9595c706050" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
