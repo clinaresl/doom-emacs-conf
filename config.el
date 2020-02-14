b4d6cd37b3ff45737ba1cb434eded1afe9f6111a
Carlos Linares López <clinaresl@eservicios.indra.es>
2020-02-14 10:01:16 +0100

    I added the configuration of golang for automatically running goimports, gofmt and also a couple of bindings for godef

--- config.el
+++ config.el
@@ -19,7 +19,31 @@
 ;; manual selection of themes
 (load-theme 'doom-spacegrey t)
 
+;; Python
+;; ----------------------------------------------------------------------------
 ;; make sure no white spaces are inserted before parenthesis in python-mode
 (add-hook
  'python-mode-hook
  (setq parens-require-spaces nil))
+
+;; Go
+;; ----------------------------------------------------------------------------
+(defun my-go-mode-hook ()
+
+  ; Use goimports instead of go-fmt
+  (setq gofmt-command "goimports")
+
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+
+  ; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "M-*") 'pop-tag-mark)
+  )
+
+(add-hook 'go-mode-hook 'my-go-mode-hook)
