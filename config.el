a7cb41051c3576c75fca1f5988f0bcdd0d8b8a18
Carlos Linares López <carlos.linares@uc3m.es>
2020-12-12 22:14:15 +0100

    Added support to template files

--- config.el
+++ config.el
@@ -26,11 +26,14 @@
 ;; make sure no white spaces are inserted before parenthesis in python-mode
 (add-hook
  'python-mode-hook
- (setq parens-require-spaces nil))
+ (lambda () (setq parens-require-spaces nil)))
 
 ;; suddenly, flycheck.el changes the checkers chain and placed flake8 before
 ;; pylint, which is rather annoying so I just removed it!
 (setq-default flycheck-disabled-checkers '(python-flake8))
+
+;; also register the local templates for python
+(set-file-template! "\\.\\(?:py\\|python\\)$" :trigger "__.python" :mode 'python-mode)
 
 ;; Go
 ;; ----------------------------------------------------------------------------
@@ -53,22 +56,6 @@
   )
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
-
-;; Lua
-;; ----------------------------------------------------------------------------
-
-;; add support for editing Lua files
-(add-to-list 'load-path "~/.emacs.d/.local/elpa/lua-mode-20200405.1305")
-
-(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
-(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
-(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
-
-;; and enable flymake for lua
-(add-to-list 'load-path "~/.emacs.d/.local/elpa/flymake-lua-20170129.154")
-(eval-after-load 'lua-mode '(add-hook 'lua-mode-hook 'flymake-lua-setup))
-(autoload #'flymake-lua-setup "flymake-lua" nil t)
-
 
 ;; Org
 ;; ----------------------------------------------------------------------------
