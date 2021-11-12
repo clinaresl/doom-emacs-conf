b5e564dfb82f4a0f3b3c04a1992e53399b436f71
Carlos Linares López <carlos.linares@uc3m.es>
2021-11-12 12:20:03 +0100

    c++ snippets

---
+++ snippets/c++-mode/__.hh
@@ -0,0 +1,23 @@
+// -*- coding: utf-8 -*-
+// `(file-name-nondirectory buffer-file-name)`
+// -----------------------------------------------------------------------------
+//
+// Started on <`(format-time-string "%a %d-%m-%Y %T.%N (%s)")`>
+// Carlos Linares López <carlos.linares@uc3m.es>
+//
+
+//
+// Description
+//
+
+#ifndef _`(upcase (file-name-nondirectory (file-name-sans-extension buffer-file-name)))`_H_
+#define _`(upcase (file-name-nondirectory (file-name-sans-extension buffer-file-name)))`_H_
+
+
+
+#endif // _`(upcase (file-name-nondirectory (file-name-sans-extension buffer-file-name)))`_H_
+
+// Local Variables:
+// mode:cpp
+// fill-column:80
+// End:
---
+++ snippets/c++-mode/__main.cc
@@ -0,0 +1,33 @@
+// -*- coding: utf-8 -*-
+// `(file-name-nondirectory buffer-file-name)`
+// -----------------------------------------------------------------------------
+//
+// Started on <`(format-time-string "%a %d-%m-%Y %T.%N (%s)")`>
+// Carlos Linares López <carlos.linares@uc3m.es>
+//
+
+//
+// Description
+//
+
+// include
+#include <iostream>
+
+// definitions
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+using namespace std;
+
+// main
+int main(int argc, char *argv[]) {
+
+
+   return EXIT_SUCCESS;
+}
+
+
+// Local Variables:
+// mode:cpp
+// fill-column:80
+// End:
